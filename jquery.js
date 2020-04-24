15333a9fb5de00c280b51de2d80b881a33182218
poyuchen <daniel7711potter@gmail.com>
2020-04-24 18:16:15 +0800

    Fixed speed -- too fast

--- jquery.js
+++ jquery.js
@@ -37,7 +37,7 @@ $(function(){
     $("#fruit1").show();
     chooseFruit();
     $("#fruit1").css({'left': Math.random() * 540, 'top': -65});
-    randomStep = Math.round(Math.random() * 5) + 1;
+    randomStep = Math.round(Math.random() * 3) + 1;
     action = setInterval(function(){
       $("#fruit1").css({'top': $("#fruit1").position().top + randomStep});
       if ($("#fruit1").position().top > $("#window").height() + 20) {
@@ -47,7 +47,7 @@ $(function(){
           $("#fruit1").show();
           chooseFruit();
           $("#fruit1").css({'left': Math.random() * 540, 'top': -65});
-          randomStep = Math.round(Math.random() * 4) + 1;
+          randomStep = Math.round(Math.random() * 3) + 1;
         } else {
           $("#counter").empty();
           playing = false;
